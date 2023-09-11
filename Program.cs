7d191a7add831c8e8e95df064fe9ef88ed65c1bd
69VR69 <44296242+69VR69@users.noreply.github.com>
2023-09-11 00:59:22 -0700

    Fix image building

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿
-using System.Text.RegularExpressions;
-
 using OpenCvSharp;
 
 using OpenCVTest;
@@ -15,7 +13,7 @@ int width, height;
 (width, height) = Utils.GetSizeFromImage(@"Resources\Michelangelo_ThecreationofAdam_1707x775.jpg");
 
 // White background
-Mat background = new(height, width, MatType.CV_8UC3, WHITE);
+Mat background = new(height, width, MatType.CV_8UC4, WHITE);
 
 // Filter fragment with fragment_s.txt
 List<int> fragmentToDelete = File.ReadAllLines(@"Resources\fragments_s.txt")
@@ -24,16 +22,16 @@ List<int> fragmentToDelete = File.ReadAllLines(@"Resources\fragments_s.txt")
 imageList.RemoveAll(f => fragmentToDelete.Contains(f.Number));
 
 // Combine fragment list and image list by number
-foreach (var fragment in fragmentList)
+foreach (Fragment frag in fragmentList)
 {
-    Fragment? image = imageList.Find(f => f.Number == fragment.Number);
-    if (image is not null)
-        fragment.Image = image?.Image;
+    Fragment? img = imageList.Find(f => f.Number == frag.Number);
+    if (img is not null)
+        frag.Image = img?.Image;
 }
 fragmentList.RemoveAll(f => f.Image is null);
 
 // Draw fragment on background
-foreach (var fragment in fragmentList)
+foreach (Fragment fragment in fragmentList)
 {
     Mat? image = fragment.Image;
     if (image is null)
@@ -44,10 +42,18 @@ foreach (var fragment in fragmentList)
     // Rotate and translate image
     Cv2.WarpAffine(image, translated, Cv2.GetRotationMatrix2D(new Point2f(image.Width / 2, image.Height / 2), fragment.Angle, 1.0), image.Size());
 
-    Console.WriteLine($"Fragment {fragment.Number} at ({fragment.X}, {fragment.Y}) with angle {fragment.Angle}\n Background [{0}->{background.Width}, {0}->{background.Height}]\n Frag [{fragment.X}->{fragment.X + translated.Width}, {fragment.Y}->{fragment.Y + translated.Height}]\n");
+    Console.WriteLine($"Fragment {fragment.Number} at ({fragment.X}, {fragment.Y}) with angle {fragment.Angle}\n Background [{0}->{background.Width}, {0}->{background.Height}]\n Frag [{fragment.X}->{fragment.X + translated.Width / 2}, {fragment.Y}->{fragment.Y + translated.Height / 2}]\n");
 
     // Add image to background
-    translated.CopyTo(background[new Rect((int)fragment.X, (int)fragment.Y, translated.Width, translated.Height)]);
+    Rect rect = new((int)fragment.X - translated.Width / 2, (int)fragment.Y - translated.Height / 2, translated.Width, translated.Height);
+    if (rect.X < 0 || rect.Y < 0 || rect.X + rect.Width > background.Width || rect.Y + rect.Height > background.Height)
+        continue;
+    var temp = background[rect];
+    Mat dst = new Mat();
+    Cv2.AddWeighted(temp, 0.0, translated, 1.0, 0.0, dst);
+
+    background[rect] = dst;
+
 
 }
 
